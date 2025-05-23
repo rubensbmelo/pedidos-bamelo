f6d97ddf045f66563cfa6d8f39e2c7c1fd86faba
Rubens Bandeira <rubensbmelo@hotmail.com>
2025-05-23 20:55:46 -0300

    Adiciona rotas com React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PedidosBamelo from './PedidosBamelo';
-import Login from './Login';
+import App from './App'; // <- aqui você aponta para o App.jsx novo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PedidosBamelo />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
